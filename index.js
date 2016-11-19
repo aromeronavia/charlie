da78153e6c146936e3be5cc0ab5c32e084cb508a
Alberto Romero Navia <lguitarras0594@gmail.com>
2016-11-18 23:45:23 -0600

    Finishes 100cia, the best rando messages tool ever

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017/test';
 const PORT = 3000;
+const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/favicon.ico', (req, res) => ({ res.sendStatus(200); }));
+const buildError = res => {
+  return res.json({message: 'khe'});
+};
 
-app.get('/', (req, res) => res.json({message: 'JAJA SALUDOS'}));
+const buildRanOutOfMessagesResponse = res => {
+  return res.json({message: 'We ran out of messages OMG!😱'});
+};
+
+const findAndDeleteRandomMessage = (db, res) => {
+  db.collection('messages').aggregate([{$sample: {size: 1}}], (err, result) => {
+    if (err) return buildError(res);
+    if (result.length === 0) return buildRanOutOfMessagesResponse(res);
+    db.collection('messages').remove({_id: result[0]._id});
+    return res.json({message: result[0].message});
+  });
+};
+
+const insertMessage = (db, res, message) => {
+  db.collection('messages').insert({message}, (err, result) => {
+    if (err) return buildError(res);
+    return res.json({status: 'ok'});
+  });
+};
+
+const controller = {
+  popMessage: (req, res) => {
+    MongoClient.connect(url, (err, db) => {
+      if (err) return buildError(res);
+      return findAndDeleteRandomMessage(db, res);
+    });
+  },
+  pushMessage: (req, res) => {
+    const { message } = req.body;
+    MongoClient.connect(url, (err, db) => {
+      if (err || !message) return buildError(res);
+      return insertMessage(db, res, message);
+    });
+  }
+}
+
+app.get('/favicon.ico', (req, res) => { 
+  return res.sendStatus(200); 
+});
+
+app.get('/', controller.popMessage);
+app.post('/', controller.pushMessage);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
